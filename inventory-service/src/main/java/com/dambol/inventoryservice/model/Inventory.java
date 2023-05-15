535ba892264d4d4c1c12b659fed0ff78765607cd
dambolewski <dambolewski@gmail.com>
2023-05-15 20:31:08 +0200

    Inventory Service added

---
+++ inventory-service/src/main/java/com/dambol/inventoryservice/model/Inventory.java
@@ -0,0 +1,19 @@
+package com.dambol.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
