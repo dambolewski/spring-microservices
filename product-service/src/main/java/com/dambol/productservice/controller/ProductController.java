d44b07a5db952d3b63ce5a412187a7d78a20507a
dambolewski <dambolewski@gmail.com>
2023-05-15 17:13:35 +0200

    Product Service with unit testing implemented

---
+++ product-service/src/main/java/com/dambol/productservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.dambol.productservice.controller;
+
+import com.dambol.productservice.dto.ProductRequest;
+import com.dambol.productservice.dto.ProductResponse;
+import com.dambol.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+}
